bfaf74be0f3f70cd3a443629f9de3f1b28a433b9
Hichami Houssam <hichami.houssam@gmail.com>
2022-02-14 14:49:01 +0100

    generate app + greeting react components

---
+++ app/assets/javascripts/components/greeting.js.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Greeting extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Greeting
+
