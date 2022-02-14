178559fbe9252d3ceff320ab727bdf08cd181bb7
Hichami Houssam <hichami.houssam@gmail.com>
2022-02-14 14:17:40 +0100

    add static view to root in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'static/index'
+  
   namespace :v1, defaults: { format: :json } do
     get 'messages', to: 'messages#index'
   end
+
+  root 'static#index'
 end
