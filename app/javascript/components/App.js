82beec40362889f02bf1df6e9542bd1de4da9915
Hichami Houssam <hichami.houssam@gmail.com>
2022-02-14 15:40:31 +0100

    add redux/configurestore.js +add  provider to app.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,15 +1,20 @@
 import React from "react"
-import PropTypes from "prop-types"
+import { Provider } from 'react-redux'
+import configureStore from '../redux/configureStore'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Greeting from './Greeting'
+
+const store = configureStore();
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={ <Greeting greeting="Homie"/> }></Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={ <Greeting greeting="Homie"/> }></Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
