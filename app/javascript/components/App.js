788ba4fa9f3dce833f44b1796a63458f2e0c9783
Hichami Houssam <hichami.houssam@gmail.com>
2022-02-14 15:20:22 +0100

    add proptypes to greeting component + add greeting component to app component + rendering app component in index static view

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Greeting from './Greeting'
 class App extends React.Component {
   render () {
     return (
-      <React.Fragment>
-      </React.Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <Greeting greeting="Homie"/> }></Route>
+        </Routes>
+      </BrowserRouter>
     );
   }
 }
